724d4200686674ae4fa8dd387d9d490006ef3525
soulelyhayden <haydensoule@outlook.com>
2022-12-20 12:59:25 -0500

    Removed redundant label

--- src/schema.tsx
+++ src/schema.tsx
@@ -1,9 +1,13 @@
 import { noteInput } from './noteInput'
 import { defineType } from 'sanity'
+import React from 'react'
 
 export default defineType({
 	name: 'note',
 	type: 'string',
 	title: 'Note',
-	components: { input: noteInput },
+	components: {
+		input: noteInput,
+		field: (props) => <div>{props.children}</div>
+},
 })
